69898514378369a93220c66b156cebf7d6953f05
Candice Hawkins <candicehawkins91git config --global user.email candicehawkins91@gmail.com>
2025-09-17 22:33:13 -0500

    level 9

--- app.js
+++ app.js
@@ -4,3 +4,12 @@ setProperty("projects", "backgroundColor", "yellow");
 
 setProperty("giBtn", "border", "3px solid red");
 setProperty("sudokuS", "border", "3px solid red");
+
+function justLog() {
+  console.log("Button clicked!");
+  setText("thankYou", "Thank you for showing interest in my capstone project!");
+}
+
+function submit() {
+  "submitBtn", "click", justLog;
+}
