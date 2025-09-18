32e8b8d710d26daba5387724b5a285841128895c
Candice Hawkins <candicehawkins91git config --global user.email candicehawkins91@gmail.com>
2025-09-17 21:18:48 -0500

    moving on

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 console.log("Hello from app.js");
 
+setProperty("projects", "backgroundColor", "yellow");
 
-setProperty("home", "backgroundColor", "yellow");
-
+setProperty("giBtn", "border", "3px solid red");
+setProperty("sudokuS", "border", "3px solid red");
